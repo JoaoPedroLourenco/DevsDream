566fe247f8040d9a74cff19cf271f7a6f3b64f44
Kob <sofhia.dias@etec.sp.gov.br>
2024-08-30 16:37:03 -0300

    card do post criado, criar o estilo do botão de pop up para login

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,13 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
+
+import styles from "./Home.module.css";
+import user from "../../assets/imgs/user.png";
 
 const Home = () => {
-  const [postTitle, setPostTitle] = useState("");
-  const [postContent, setPostContent] = useState("");
-
   return (
-    <div>
+    <div className={styles.homeContainer}>
       <div className="title">
         <h1>Home</h1>
+      </div>
+
+      <div className={styles.postsContainer}>
+        <div className={styles.postCard}>
+          <div className={styles.postContent}>
+            <div className={styles.postText}>
+              <div className={styles.userName}>
+                <img src={user} alt="" />
+                <p>João Pedro L.</p>
+              </div>
+              <h2>Título do Post muito foda woow</h2>
+
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Repellendus beatae adipisci aliquid odit, nemo fugit nostrum,
+                voluptatibus sed architecto quisquam numquam deleniti, fugiat
+                molestiae!Lorem Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Fugit maiores omnis eos repellat optio officia
+                labore quis amet odit dicta ipsa esse, magnam ut tempora aliquid
+                adipisci iusto iure autem? Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Dolorum dolorem maxime, eligendi
+                sed architecto ut maiores tempora? Adipisci doloribus fuga a
+                iusto impedit porro eveniet doloremque laborum animi ipsam.
+                Repellat! Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Harum itaque sapiente amet aliquid ipsam dolorum magni
+                impedit sequi. Cupiditate ex corrupti illo rerum veniam suscipit
+                alias expedita eveniet omnis libero!
+              </p>
+            </div>
+
+            <div className={styles.postTags}>
+              <p>JS, FrontEnd, woow</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
